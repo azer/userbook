918627d52eee4f49ae28f01d3b6332a3c6340c3e
E.Azer Koçulu <azer@kodfabrik.com>
2014-02-25 00:41:00 -0800

    save authkey

--- test/prefix.js
+++ test/prefix.js
@@ -1,10 +1,10 @@
-var users = require("../")({ prefix: 'people:' });
+var users = require("../")({ prefix: 'people' });
 
 it('chooses a custom prefix rather than "users:"', function(done){
   users.create({ email: 'hi@azer.com', password: 'abcdef' }, function (error, user) {
     if (error) return done(error);
 
-    users.io.get('people:hi@azer.com', function (error, sameuser) {
+    users.io.get('people:email:hi@azer.com', function (error, sameuser) {
       if (error) return done(error);
 
       expect(user.authkey).to.equal(sameuser.authkey);
